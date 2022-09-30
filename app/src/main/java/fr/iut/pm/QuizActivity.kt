f839c75ea837ac68b8da474d0465da26d1bd936c
Alexis Drai <drai.alexis@gmail.com>
2022-09-30 17:33:32 +0200

    oih

---
+++ app/src/main/java/fr/iut/pm/QuizActivity.kt
@@ -0,0 +1,11 @@
+package fr.iut.pm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
