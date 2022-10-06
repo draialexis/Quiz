0d3f8d6f6a61303fbbca194b5bd9fe760bdbe423
Alexis Drai <drai.alexis@gmail.com>
2022-10-06 09:39:24 +0200

    :truck: Link Cheat activity to its layout

--- app/src/main/java/fr/iut/pm/CheatActivity.kt
+++ app/src/main/java/fr/iut/pm/CheatActivity.kt
@@ -1,7 +1,11 @@
 package fr.iut.pm
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class CheatActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cheat_activity)
+    }
 }
